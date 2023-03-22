96b4d4aa6d0b48efba8f31eb624d5cacc046faf6
Vinesh Raju <hi@thevinesh.com>
2023-03-22 18:40:25 +0000

    Change user agent if its bing

--- background.js
+++ background.js
@@ -1,29 +1,31 @@
-let currentUrl;
+let isEnabled = true;
 
-function updateCurrentUrl() {
-    chrome.tabs.query({ active: true }, tabs => {
-        currentUrl = tabs[0].url;
-    });
-}
-
-// This event is fired when a tab is updated 
-// e.g., when the user navigates to a new link or refreshes the page
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-        updateCurrentUrl();
-    }
-});
-
-// This event is fired when a tab is activated 
-// e.g., when the user switches to a different tab.
-chrome.tabs.onActivated.addListener(activeInfo => {
-    updateCurrentUrl();
-});
+const keyword = "bing.com";
+const edgeUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/604.1 Edg/111.0.100.0";
 
 // This event is fired when a message is sent from either an extension process 
 // or a content script.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'getCurrentUrl') {
-        sendResponse({ url: currentUrl });
+    if (request.type === 'toggle') {
+        sendResponse({ enabled: isEnabled = !isEnabled });
+    }
+    if (request.type === 'isEnabled') {
+        sendResponse({ enabled: isEnabled });
     }
 });
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    details => {
+        if (isEnabled && details.url.includes(keyword)) {
+            for (let header of details.requestHeaders) {
+                if (header.name.toLowerCase() === "user-agent") {
+                    header.value = edgeUserAgent;
+                    break;
+                }
+            }
+        }
+        return { requestHeaders: details.requestHeaders };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking", "requestHeaders"]
+);
--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.runtime.sendMessage({type: 'getCurrentUrl'}, response => {
-        document.getElementById('current-url').textContent = response.url;
+    chrome.runtime.sendMessage({type: 'isEnabled'}, response => {
+        document.getElementById('toggle').textContent = response.enabled;
+    });
+    document.getElementById('toggle').addEventListener('click', () => {
+        chrome.runtime.sendMessage({type: 'toggle'}, response => {
+            document.getElementById('toggle').textContent = response.enabled;
+        });
     });
 });
