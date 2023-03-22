c137f2b631cd5cb880cf44f82bc64dddba5e4f75
Vinesh Raju <hi@thevinesh.com>
2023-03-22 21:12:29 +0000

    Ui Change to popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.runtime.sendMessage({type: 'isEnabled'}, response => {
-        document.getElementById('toggle').textContent = response.enabled;
+    chrome.runtime.sendMessage({ type: 'isEnabled' }, response => {
+        document.getElementById('status').textContent = getStatusMessage(response.enabled);
+        document.getElementById('toggle').textContent = getCTAText(response.enabled);
     });
     document.getElementById('toggle').addEventListener('click', () => {
-        chrome.runtime.sendMessage({type: 'toggle'}, response => {
-            document.getElementById('toggle').textContent = response.enabled;
+        chrome.runtime.sendMessage({ type: 'toggle' }, response => {
+            document.getElementById('toggle').textContent = getCTAText(response.enabled);
         });
     });
 });
+
+function getCTAText(status) {
+    return status ? 'Disable' : 'Enable';
+}
+
+function getStatusMessage(status) {
+    return status ? 'Enabled' : 'Disabled';
+}
